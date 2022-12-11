a1a9d47027c72dde6261d17fa20d627781b03834
kulmika4 <kulmika4@gmail.com>
2022-12-11 21:40:35 +0100

    init

---
+++ CSHill/index.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using SimpleTCP;
+
+class Server
+{
+    static void Main() {
+        var server = new SimpleTcpServer();
+
+        server.ClientConnected += (sender, e) =>
+        {
+            Console.WriteLine($"Client ({e.Client.RemoteEndPoint}) connected!");
+            Console.WriteLine(e);
+        };
+
+        server.ClientDisconnected += (sender, e) => Console.WriteLine($"Client ({e.Client.RemoteEndPoint}) disconnected!");
+
+        server.DataReceived += (sender, e) =>
+        {
+            foreach(var data in e.Data)
+                Console.Write(data.ToString()+" ");
+            Console.WriteLine();
+            //Console.WriteLine(e.Data.ToString());
+            //Console.WriteLine(e.TcpClient.Client);
+        };
+
+        server.Start(42480);
+
+        while (true)
+        {
+
+        }
+    } 
+}
+
+
+
+
---
+++ CSHill/startclient.bat
@@ -0,0 +1,5 @@
+@echo off
+cd %appdata%\Brick Hill
+
+start "" c:Player.exe "local/local/42480" 
+exit
